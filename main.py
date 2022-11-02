82c29bd54b30e797db3d9c1fa14bdd627f45cf7d
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-02 21:43:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -49,8 +49,8 @@ Model_push = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/
 KM_push = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[4]/div/div[1]/input').send_keys("100000")
 #Price_input = input("Wie viel soll er kosten ? :")
 Price_push = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[6]/div/div[1]/input').send_keys("35000")
-#GPS_butt = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[7]/span/span')
-#GPS_butt.click()
+GPS_butt = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[7]/span/span')
+GPS_butt.click()
 sleep(3.0)
 
 # Funktion für die Plausibilisierung
@@ -63,7 +63,7 @@ if valueget > "0":
     next_c.click()
     sleep(8.0)
 
-    get_URL = driver.find_element(By.XPATH, '//*[@id="main-header"]/div[2]/nav/div/ul[1]/li[3]/ul/li[1]/a').get_attribute("href")
+    get_URL = driver.find_element(By.XPATH, '//*[@id="main-header"]/div[2]/nav/div/ul[1]/li[3]/ul/li[1]').get_attribute("href")
 
     resp = client.send_message("NEW_Car", "Neues Auto", "a", "1", "4", "2", get_URL ,
                                  "Mobile öffnen!", "0", "2", "60", "600", "1", "", "", "")
