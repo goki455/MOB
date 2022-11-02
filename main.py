191c34dc8d2da887851d18a0f64fdd20e0e98126
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-02 14:50:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--ignore-certificate-errors')
-browser = webdriver.Chrome(options=chrome_options)
+driver = webdriver.Chrome(options=chrome_options)
 
 
 
