20675864cae9ff10e477b4c7a9b67e8b00e96afd
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-03 16:25:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,11 +18,12 @@ chromedriver_autoinstaller.install()
 #Chrome options
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--window-size=1920,1080")
+
 chrome_options.add_argument("--start-maximized")
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--headless')
+chrome_options.add_argument("--window-size=1920,1080")
 chrome_options.add_argument('--ignore-certificate-errors')
 driver = webdriver.Chrome(options=chrome_options)
 
