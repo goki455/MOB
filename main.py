b0951a5b701e0150c29afef92d9d28e95038e97e
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-03 22:33:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ driver.get(Url)
 sleep(10.0)
 def Try():
     try:
-        buttonc = driver.find_element(By.selector, '#mde-consent-modal-container > div.sc-iBkjds.frmJSB > div.sc-papXJ.ixHhna > div.sc-jqUVSM.dBIrCF > button')
+        buttonc = driver.find_element(By.SELECTOR, '#mde-consent-modal-container > div.sc-iBkjds.frmJSB > div.sc-papXJ.ixHhna > div.sc-jqUVSM.dBIrCF > button')
         buttonc.click()
     except:
         return
@@ -59,7 +59,7 @@ sleep(5.0)
 sleep(5.0)
 #Marke_input=input("Welche Marke? :")
 #Marke_pusc = driver.find_element(By.CSS_SELECTOR , '#root > div.hp7JS > div > article.RSseD._3LZ_7._2iEKW > section > div > div.UiAUP > div > div:nth-child(1) > div > select')
-Marke_push = driver.find_element(By.selector, '#root > div > div > article.RSseD._3LZ_7._2iEKW > section > div > div.UiAUP > div > div:nth-child(1) > div > select')
+Marke_push = driver.find_element(By.SELECTOR, '#root > div > div > article.RSseD._3LZ_7._2iEKW > section > div > div.UiAUP > div > div:nth-child(1) > div > select')
 Marke_push.send_keys("Audi")
 Marke_push.click()
 
