0fb52658b1d280621912404d73d6e0ceb469e502
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-03 14:58:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ buttonc.click()
 sleep(2.0)
 #Marke_input=input("Welche Marke? :")
 Marke_push = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[1]/div/select')
-Marke_pusc = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[1]/div/svg)
+Marke_pusc = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[1]/div/svg')
 Marke_pusc.click()
 Marke_push.send_keys("Audi")
 
