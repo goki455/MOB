042e6d2400b477d7b07e79316dcf846aa1059dd3
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-03 09:16:38 +0100

    Update main.py
    Try if

--- main.py
+++ main.py
@@ -44,38 +44,40 @@ buttonc.click()
 sleep(2.0)
 #Marke_input=input("Welche Marke? :")
 Marke_push = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[1]/div/select')
-Marke_push.send_keys("Audi")
-sleep(2.0)
-Model_push = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[2]/div/select')
-Model_push.send_keys("S3")
-sleep(2.0)
-KM_push = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[4]/div/div[1]/input')
-KM_push.send_keys("100000")
-sleep(2.0)
-Price_push = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[6]/div/div[1]/input')
-Price_push.send_keys("9000")
+if Marke_push == True:
+    print("True)
+#Marke_push.send_keys("Audi")
+#sleep(2.0)
+#Model_push = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[2]/div/select')
+#Model_push.send_keys("S3")
+#sleep(2.0)
+#KM_push = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[4]/div/div[1]/input')
+#KM_push.send_keys("100000")
+#sleep(2.0)
+#Price_push = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[6]/div/div[1]/input')
+#Price_push.send_keys("9000")
 #GPS_butt = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[7]/span/span')
 #GPS_butt.click()
-sleep(3.0)
+#sleep(3.0)
 
 # Funktion für die Plausibilisierung
 
-span_element = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[8]/button/span/span/span')
-valueget = (span_element.text[0])
-sleep(4.0)
+#span_element = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[8]/button/span/span/span')
+#valueget = (span_element.text[0])
+#sleep(4.0)
 
-if valueget >= "0":
-    sleep(8.0)
-    next_c = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[8]/button/span').click()
-    sleep(50.0)
-    driver.find_element(By.XPATH, '//*[@id="main-header"]/div[2]/nav/div/ul[1]/li[3]/ul/li[1]/a').get_attribute("href")
+#if valueget >= "0":
+ #   sleep(8.0)
+  #  next_c = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[8]/button/span').click()
+   # sleep(10.0)
+   # driver.find_element(By.XPATH, '//*[@id="main-header"]/div[2]/nav/div/ul[1]/li[3]/ul/li[1]/a').get_attribute("href")
 
-    resp = client.send_message("NEW_Car", "Neues Auto", "a", "1", "4", "2", driver.find_element(By.XPATH, '//*[@id="main-header"]/div[2]/nav/div/ul[1]/li[3]/ul/li[1]/a').get_attribute("href") ,
+    #resp = client.send_message("NEW_Car", "Neues Auto", "a", "1", "4", "2", driver.find_element(By.XPATH, '//*[@id="main-header"]/div[2]/nav/div/ul[1]/li[3]/ul/li[1]/a').get_attribute("href") ,
                                  "Mobile öffnen!", "0", "2", "60", "600", "1", "", "", "")
 
 
-else:
-    print("Keine Ergebnisse")
+#else:
+ #   print("Keine Ergebnisse")
 
 
 
