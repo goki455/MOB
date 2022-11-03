bfedc9e9035497c6c2cd697fb422d1ae203cab4f
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-03 06:31:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -62,6 +62,7 @@ valueget = (span_element.text[0])
 if valueget >= "0":
     next_c = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[8]/button')
     next_c.click()
+    driver.save_screenshot("image.png")
     sleep(8.0)
 
     get_URL = driver.find_element(By.XPATH, '//*[@id="main-header"]/div[2]/nav/div/ul[1]/li[3]/ul/li[1]/a').get_attribute("href")
