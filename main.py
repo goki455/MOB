a2bd6bfcfd2e8dc97b18961961c07f77b062824f
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-03 07:59:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -58,15 +58,15 @@ sleep(3.0)
 
 span_element = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[8]/button/span/span/span')
 valueget = (span_element.text[0])
-
+sleep(4.0)
 
 if valueget >= "0":
     sleep(8.0)
     next_c = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[8]').click()
     sleep(8.0)
-    get_URL = driver.find_element(By.XPATH, '//*[@id="main-header"]/div[2]/nav/div/ul[1]/li[3]/ul/li[1]/a').get_attribute("href")
+    driver.find_element(By.XPATH, '//*[@id="main-header"]/div[2]/nav/div/ul[1]/li[3]/ul/li[1]/a').get_attribute("href")
 
-    resp = client.send_message("NEW_Car", "Neues Auto", "a", "1", "4", "2", get_URL ,
+    resp = client.send_message("NEW_Car", "Neues Auto", "a", "1", "4", "2", driver.find_element(By.XPATH, '//*[@id="main-header"]/div[2]/nav/div/ul[1]/li[3]/ul/li[1]/a').get_attribute("href") ,
                                  "Mobile öffnen!", "0", "2", "60", "600", "1", "", "", "")
 
 
