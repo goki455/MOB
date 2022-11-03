6487d35f7635c39fd94dbbe7d001ce222689de31
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-03 14:08:01 +0100

    Update main.py
    old version

--- main.py
+++ main.py
@@ -1,40 +1,60 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@goki455 
+goki455
+/
+MOB
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+MOB/main.py /
+@goki455
+goki455 Update main.py
+Latest commit cf3eee0 5 hours ago
+ History
+ 1 contributor
+77 lines (51 sloc)  2.76 KB
+
 from selenium import webdriver
 from pushsafer import Client
 from selenium.webdriver.common.by import By
-import webdrivermanager
-from time import sleep
-import packaging
-from selenium.webdriver.common.keys import Keys
-from selenium import webdriver
+import chromedriver_autoinstaller
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
+from time import sleep
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 
 #Chrome options
 
 
 
 client = Client("bjHKdba7y0Hj8LRlRs3i")
-driver_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-driver = webdriver.Chrome(driver_path)
 
 
-chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+chromedriver_autoinstaller.install()
+#Chrome options
 
-chrome_options = Options()
-options = [
-    "--headless",
-    "--disable-gpu",
-    "--window-size=1920,1200",
-    "--ignore-certificate-errors",
-    "--disable-extensions",
-    "--no-sandbox",
-    "--disable-dev-shm-usage"
-]
-for option in options:
-    chrome_options.add_argument(option)
-
-driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--ignore-certificate-errors')
+driver = webdriver.Chrome(options=chrome_options)
 
 
 
@@ -52,24 +72,16 @@ buttonc = driver.find_element(By.XPATH , '//*[@id="mde-consent-modal-container"]
 buttonc.click()
 sleep(2.0)
 #Marke_input=input("Welche Marke? :")
-Marke_push = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[1]/div/select')
-if Marke_push == True:
-    print("True")
-else:
-    print(Marke_push)
-Marke_push.send_keys("Audi")
-sleep(2.0)
-Model_push = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[2]/div/select')
-Model_push.send_keys("S3")
-sleep(2.0)
-KM_push = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[4]/div/div[1]/input')
-KM_push.send_keys("100000")
-sleep(2.0)
-Price_push = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[6]/div/div[1]/input')
-Price_push.send_keys("9000")
+Marke_push = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[1]/div').send_keys("Audi")
+#Model_input = input("Welches Modell ? :")
+Model_push = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[2]/div/select').send_keys("S3")
+#KM_input = input("Wie viel Kilometer ? :")
+KM_push = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[4]/div/div[1]/input').send_keys("100000")
+#Price_input = input("Wie viel soll er kosten ? :")
+Price_push = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[6]/div/div[1]/input').send_keys("35000")
 #GPS_butt = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[7]/span/span')
 #GPS_butt.click()
-#sleep(3.0)
+sleep(3.0)
 
 # Funktion für die Plausibilisierung
 
@@ -79,8 +91,8 @@ sleep(4.0)
 
 if valueget >= "0":
     sleep(8.0)
-    next_c = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[8]/button/span').click()
-    sleep(10.0)
+    next_c = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[8]').click()
+    sleep(8.0)
     driver.find_element(By.XPATH, '//*[@id="main-header"]/div[2]/nav/div/ul[1]/li[3]/ul/li[1]/a').get_attribute("href")
 
     resp = client.send_message("NEW_Car", "Neues Auto", "a", "1", "4", "2", driver.find_element(By.XPATH, '//*[@id="main-header"]/div[2]/nav/div/ul[1]/li[3]/ul/li[1]/a').get_attribute("href") ,
@@ -89,6 +101,5 @@ if valueget >= "0":
 
 else:
     print("Keine Ergebnisse")
-
 
 
