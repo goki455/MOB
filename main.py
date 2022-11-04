6d069daa995773eaf65a3c33974f55366b9255eb
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-04 10:18:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import chromedriver_autoinstaller
 from selenium.webdriver.chrome.service import Service
 from time import sleep
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 
 
@@ -58,6 +59,7 @@ sleep(5.0)
 
 sleep(5.0)
 #Marke_input=input("Welche Marke? :")
+WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH,"//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[1]/div"))).click()
 #Marke_pusc = driver.find_element(By.CSS_SELECTOR , '#root > div.hp7JS > div > article.RSseD._3LZ_7._2iEKW > section > div > div.UiAUP > div > div:nth-child(1) > div > select')
 Marke_push = driver.find_element(By.XPATH , '//*[@id="root"]/div/div/article[1]/section/div/div[2]/div/div[1]/div')
 Marke_push.click()
