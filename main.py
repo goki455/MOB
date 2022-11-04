35b2d39c5187b6b934bfbce23883ea14be7a33f6
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-04 10:35:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 #Chrome options
 
