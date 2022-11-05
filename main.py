8992f5d0116247683800cde7ecd9881f209e34a0
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-05 20:51:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ from selenium import webdriver
 from pushsafer import Client
 from selenium.webdriver.common.by import By
 import chromedriver_autoinstaller
-from selenium.webdriver.chrome.service import Service
 from time import sleep
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
+
 
 #Chrome options#
 client = Client("bjHKdba7y0Hj8LRlRs3i")
@@ -41,7 +38,8 @@ driver = webdriver.Chrome(options=options1)
 Url = ("https://suchen.mobile.de/fahrzeuge/search.html?dam=0&sb=rel&vc=Car")
 
 driver.get(Url)
-
+#buttonc = driver.find_element(By.XPATH, '//*[@id="mde-consent-modal-container"]/div[2]/div[2]/div[1]/button')
+#buttonc.click()
 sleep(3.0)
 def Try():
     try:
@@ -54,63 +52,63 @@ Try()
 #buttonc.click()
 
 
+def AUDISEARCH():
 
 
-def audisel():                      #Marke_input=input("Welche Marke? :")
-    WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="selectMake1-ds"]'))).click()
-    clic_Marke = driver.find_element(By.XPATH, '//*[@id="selectMake1-ds"]/option[12]')
-    clic_Marke.click()
-audisel()
+    def audisel():                      #Marke_input=input("Welche Marke? :")
+        WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="selectMake1-ds"]'))).click()
+        clic_Marke = driver.find_element(By.XPATH, '//*[@id="selectMake1-ds"]/option[12]')
+        clic_Marke.click()
+    audisel()
 
 
-sleep(2.0)
-#Marke_push.click()
-def models3():          #Model_input = input("Welches Modell ? :")
+    sleep(2.0)
+    #Marke_push.click()
+    def models3():          #Model_input = input("Welches Modell ? :")
 
-    #WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="selectModel1-ds"]'))).click()
-    Model_push = driver.find_element(By.XPATH , '//*[@id="selectModel1-ds"]')
-    #Model_push.click()
-    Model_push.send_keys("S3")
-models3()
-def kms3():#KM_input = input("Wie viel Kilometer ? :")
-    KM_push = driver.find_element(By.XPATH , '//*[@id="maxMileage"]')
-    KM_push.send_keys("100000")
-kms3()
-def prices3():#("Wie viel soll er kosten ? :") (5000€) Click
-    Price_push = driver.find_element(By.XPATH , '//*[@id="payment-filters"]/div[2]/div/div[2]/div/div[2]/select/option[11]')
-    Price_push.click()
-
-
-    sleep(3.0)
-prices3()
-# Funktion für die Plausibilisierung
-def prufung():
-    span_element = driver.find_element(By.XPATH, '//*[@id="dsp-upper-search-btn"]/span/span')
-    global valueget
-    valueget = (span_element.text[0])
-    sleep(7.0)
-prufung()
-def returnthevalue():
-
-    if valueget > "0":
-        next_c = driver.find_element(By.XPATH, '//*[@id="dsp-upper-search-btn"]')
-        next_c.click()
-        sleep(5.0)
-
-        get_URL = driver.current_url
-
-        resp = client.send_message("NEW_Car", "Neues Auto", "a", "1", "4", "2",get_URL,
-        "Mobile öffnen!", "0", "2", "60", "600", "1", "", "", "")
-        
+        WebDriverWait(driver,5).until(EC.element_to_be_clickable((By.XPATH,'//*[@id="selectModel1-ds"]'))).click()
+        Model_push = driver.find_element(By.XPATH , '//*[@id="selectModel1-ds"]')
+        #Model_push.click()
+        Model_push.send_keys("S3")
+    models3()
+    def kms3():#KM_input = input("Wie viel Kilometer ? :")
+        KM_push = driver.find_element(By.XPATH , '//*[@id="maxMileage"]')
+        KM_push.send_keys("100000")
+    kms3()
+    def prices3():#("Wie viel soll er kosten ? :") (5000€) Click
+        Price_push = driver.find_element(By.XPATH , '//*[@id="payment-filters"]/div[2]/div/div[2]/div/div[2]/select/option[11]')
+        Price_push.click()
 
 
+        sleep(3.0)
+    prices3()
+    # Funktion für die Plausibilisierung
+    def prufung():
+        span_element = driver.find_element(By.XPATH, '//*[@id="dsp-upper-search-btn"]/span/span')
+        global valueget
+        valueget = (span_element.text[0])
+        sleep(3.0)
+    prufung()
+    def returnthevalue():
+        if  valueget > "0":
+            next_c = driver.find_element(By.XPATH, '//*[@id="dsp-upper-search-btn"]')
+            next_c.click()
+            sleep(5.0)
 
+            get_URL = driver.current_url
 
-    else:
-        print("Keine Ergebnisse")
+            resp = client.send_message("NEW_Car", "Neues Auto", "a", "1", "4", "2",get_URL,
+            "Mobile öffnen!", "0", "2", "60", "600", "1", "", "", "")
 
 
 
-returnthevalue()
 
 
+        else:
+            print("Keine Ergebnisse")
+
+
+
+    returnthevalue()
+
+AUDISEARCH()
