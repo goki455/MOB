224033da283f4d2177b74181981b3faf20ae0f68
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-06 07:50:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -107,13 +107,14 @@ def AUDISEARCH():
 
         client.send_message("NEW_Car", "Neues Auto", "a", "1", "4", "2", get_URL,
                                 "Mobile öffnen!", "0", "2", "60", "600", "1", "", "", "")
-
-
-
-
-
         else:
           return
+
+
+
+
+
+         
 
     returnthevalue()
 
