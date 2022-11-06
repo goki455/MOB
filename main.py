0d255b59e274618c92d2416a257bc51ca326d315
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-06 13:32:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ def AUDISEARCH():
 
     def prices3():  # ("Wie viel soll er kosten ? :") (5000€) Click
         Price_push = driver.find_element(By.XPATH,
-                                         '//*[@id="payment-filters"]/div[2]/div/div[2]/div/div[2]/select/option[15]')
+                                         '//*[@id="payment-filters"]/div[2]/div/div[2]/div/div[2]/select/option[11]')
         Price_push.click()
         sleep(1.0)
     
@@ -98,7 +98,7 @@ def AUDISEARCH():
         #print(valueget)
 
     
-    #driver.refresh()
+    driver.refresh()
     sleep(1.0)
 
       
