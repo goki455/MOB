0ea3386dff1f63801f54f57098954379d6bd5072
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-06 08:04:24 +0100

    Update main.py
    syntax fix

--- main.py
+++ main.py
@@ -97,18 +97,15 @@ def AUDISEARCH():
     prufung()
 
     def returnthevalue():
-      
-
-    if valueget > "0":
-
-      next_c = driver.find_element(By.XPATH, '//*[@id="dsp-upper-search-btn"]')
-      next_c.click()
-      sleep(5.0)
-      get_URL = driver.current_url
-      client.send_message("NEW_Car", "Neues Auto", "a", "1", "4", "2", get_URL,
-                              "Mobile öffnen!", "0", "2", "60", "600", "1", "", "", "")
-    else:
-      return
+      if valueget > "0":
+        next_c = driver.find_element(By.XPATH, '//*[@id="dsp-upper-search-btn"]')
+        next_c.click()
+        sleep(5.0)
+        get_URL = driver.current_url
+        client.send_message("NEW_Car", "Neues Auto", "a", "1", "4", "2", get_URL,
+                                "Mobile öffnen!", "0", "2", "60", "600", "1", "", "", "")
+      else:
+        return
 
 
 
