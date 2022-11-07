6eaeeda3a19166bf6e0471175acdd750a245d9d2
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-07 09:56:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -80,13 +80,13 @@ def AUDISEARCH():
 
     def prices3():  # ("Wie viel soll er kosten ? :") (5000€) Click
         #WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="payment-filters"]/div[2]/div/div[2]/div/div[1]/input'))).click()
-        sel = Select(driver.find_element(By.XPATH, '//*[@id="payment-filters"]/div[2]/div/div[2]/div/div[2]/select'))
-        sel.select_by_visible_text("5.000 €")
+        #sel = Select(driver.find_element(By.XPATH, '//*[@id="payment-filters"]/div[2]/div/div[2]/div/div[2]/select'))
+        #sel.select_by_visible_text("5.000 €")
         
       
         
-        #needthisaswell = driver.find_element(By.XPATH, '//*[@id="payment-filters"]/div[2]/div/div[2]/div/div[2]/select/option[11]')
-        #needthisaswell.click()
+        needthisaswell = driver.find_element(By.XPATH, '//*[@id="payment-filters"]/div[2]/div/div[2]/div/div[2]/select/option[15]')
+        needthisaswell.click()
         
         sleep(1.0)
         #clickit = driver.find_element(By.XPATH, '//*[@id="form-dsp"]/div[2]/div/div[1]/div[2]/div[1]/div[2]/div/a')
