a946c68d711ae52cfef482dbeec3590cf0003bec
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-07 12:30:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -202,8 +202,8 @@ def SEATSEARCH():
       driver.find_element(By.XPATH, '//*[@id="dsp-upper-search-btn"]').click()
       sleep(4.5)
       URL2 = driver.current_url
-      driver.get(URL2)
-      eleget2 = driver.find_element(By.XPATH, '//*[@id="minisearch-search-btn"]').text[0]
+      driver2.get(URL2)
+      eleget2 = driver2.find_element(By.XPATH, '//*[@id="minisearch-search-btn"]').text[0]
       print(eleget2)
       if eleget2 > "0":
         print(eleget2)
