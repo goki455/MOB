f5ad512dd6330d428c50e54acc530a5432129e91
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-08 07:31:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -115,20 +115,13 @@ def AUDISEARCH():
       
       
 
-    def returnthevalue():
-      needthis = driver.find_element(By.XPATH, '//*[@id="dsp-lower-search-btn"]/span/span').text
-      #global eleget
-      eleget = needthis
-      print(eleget)
-      if eleget > "0":
-        
-        
+    def returnthevalue():     
         driver.find_element(By.XPATH, '//*[@id="dsp-upper-search-btn"]').click()
         sleep(4.5)
         URL2 = driver.current_url
         driver2.get(URL2)
-        eleget2 = driver2.find_element(By.XPATH, '//*[@id="minisearch-search-btn"]')
-        eleget2.text[0]
+        eleget2 = driver2.find_element(By.XPATH, '//*[@id="minisearch-search-btn"]').text[0]
+    
         print(eleget2)
         if eleget2 != "0":
           print(eleget2)
