f91f688fec1736d5a09d10e51eeeccf5e4ed131c
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-08 10:18:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -125,8 +125,9 @@ def AUDISEARCH():
     
         print(eleget2)
         if eleget2 != "0":
-          print(eleget2)
+          
           get_URL = driver2.current_url
+          print(get_URL)
           client.send_message("AUDI!", "Neuer Audi", "a", "1", "4", "2", get_URL,
                                 "Mobile öffnen!", "0", "2", "60", "600", "1", "", "", "")
           driver.close()
