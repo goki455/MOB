46bd6c8e358f8d9ecac726c88ccc1a7eca03b142
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-08 10:23:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -125,9 +125,7 @@ def AUDISEARCH():
     
         print(eleget2)
         if eleget2 != "0":
-          
           get_URL = driver2.current_url
-          print(get_URL)
           client.send_message("AUDI!", "Neuer Audi", "a", "1", "4", "2", get_URL,
                                 "Mobile öffnen!", "0", "2", "60", "600", "1", "", "", "")
           driver.close()
@@ -218,7 +216,6 @@ def SEATSEARCH():
     
         print(eleget2)
         if eleget2 != "0":
-          print(eleget2)
           get_URL = driver2.current_url
           client.send_message("SEAT!", "LEON", "a", "1", "4", "2", get_URL,
                                 "Mobile öffnen!", "0", "2", "60", "600", "1", "", "", "")
