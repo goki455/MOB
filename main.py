4f848a409d6b1cfbe71c167c5fe1f8bfca02297b
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-08 20:24:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ options1.add_argument("--disable-gpu")
 options1.add_argument("--window-size=1920,1080")
 options1.add_argument("no-sandbox")
 options1.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
-                 #     '(KHTML, like Gecko) Chrome/107.0.5304.88 Safari/537.36')
+                      '(KHTML, like Gecko) Chrome/107.0.5304.88 Safari/537.36')
 options1.add_argument('--disable-blink-features=AutomationControlled')
 options1.add_argument('--ignore-certificate-errors')
 options1.add_argument('--allow-running-insecure-content')
