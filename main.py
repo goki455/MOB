3e6c07a3f72ca72c6bfb8a3c5628f37a2555b93f
goki455 <117120335+goki455@users.noreply.github.com>
2022-11-10 09:33:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -194,11 +194,11 @@ def SEATSEARCH():
 
     # Marke_push.click()
     def mleon():  # Model_input = input("Welches Modell ? :")
-        Model_push1 = driver.find_element(By.XPATH, '//*[@id="selectModel1-ds"]/option[11]')
+        Model_push1 = driver.find_element(By.XPATH, '//*[@id="selectModel1-ds"]')
         driver.title
         print(driver.title)
         # Model_push.click()
-        Model_push1.click()
+        Model_push1.send_keys("Leon")
         delay1
 
     mleon()
